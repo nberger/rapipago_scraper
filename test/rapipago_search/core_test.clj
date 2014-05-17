f7f3d09289f82a33780e6c30855855c1534b577a
Nicolas Berger <nicoberger@gmail.com>
2014-05-17 12:19:55 -0300

    Builds lazy-seq of stores by fetching successive pages

--- test/rapipago_search/core_test.clj
+++ test/rapipago_search/core_test.clj
@@ -7,23 +7,26 @@
 
 (deftest search-without-filter
   (with-cassette :search-without-filter
-    (let [search-result (search {})]
-      (is (= (count search-result) 6))
+    (let [search-result (take 20 (search {}))]
+      (is (= (count search-result) 20))
       (is (some #{"LOCUTORIOS INTERVISION"} (map :name search-result))))))
 
 (deftest search-with-province-filter
   (with-cassette :search-with-province-filter
     (let [province (first (provinces/find-by-name "Corrientes"))
-          search-result (search {:province province})]
-      (is (= (count search-result) 6))
+          search-result (take 20 (search {:province province}))]
+      (is (= (count search-result) 20))
       (is (not-any? #{"LOCUTORIOS INTERVISION"} (map :name search-result)))
-      (is (some #{"SABA COMUNICACIONES"} (map :name search-result))))))
+      ; checks result from 1st page
+      (is (some #{"SABA COMUNICACIONES"} (map :name search-result)))
+      ; checks result from 3rd page
+      (is (some #{"AYALA ALICIA SUSAN"} (map :name search-result))))))
 
 (deftest search-with-city-filter
   (with-cassette :search-with-city-filter
     (let [province (first (provinces/find-by-name "Corrientes"))
           city (->> province cities/find-in-province (filter #(= (:name %) "GOYA")) first)
           search-result (search {:province province :city city})]
-      (is (= (count search-result) 3))
+      (is (= (count search-result) 4))
       (is (not-any? #{"SABA COMUNICACIONES"} (map :name search-result)))
       (is (some #{"SUPERMERCADO JESUS"} (map :name search-result))))))
