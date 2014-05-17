1a34be5d1012e30defda4f29723e6655e8902398
Nicolas Berger <nicoberger@gmail.com>
2014-05-17 13:27:32 -0300

    Prepare for release 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject rapipago_scraper "0.1.0-SNAPSHOT"
+(defproject rapipago_scraper "0.1.0"
   :description "Rapipago stores scraper. Scrapes from www.rapipago.com"
   :url "http://github.com/nberger/rapipago_scraper"
+  :scm {:name "git"
+        :url "https://github.com/nberger/rapipage_scraper"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
