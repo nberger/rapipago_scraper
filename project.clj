4df82fcc08f9583d74ab88d6e73b34bcc7f24c2b
Nicolas Berger <nicoberger@gmail.com>
2016-03-02 15:52:26 -0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
         :url "https://github.com/nberger/rapipage_scraper"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.0"]
-                 [enlive "1.1.5"]
-                 [com.gfredericks/vcr-clj "0.3.3"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.1.0"]
+                 [enlive "1.1.6"]
+                 [com.gfredericks/vcr-clj "0.4.6"]])
