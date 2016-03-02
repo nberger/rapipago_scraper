bb08b0543878f6c276fb229ca94b20538bcfa8d5
Nicolas Berger <nicoberger@gmail.com>
2016-03-02 16:23:13 -0300

    Move vcr-clj to dev profile

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.1.0"]
-                 [enlive "1.1.6"]
-                 [com.gfredericks/vcr-clj "0.4.6"]])
+                 [enlive "1.1.6"]]
+  :profiles {:dev
+             {:depedencies [[com.gfredericks/vcr-clj "0.4.6"]]}})
